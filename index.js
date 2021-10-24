0e95f29b5be8375feb49781cc9e240353ee72220
Adel Soltane <Adel>
2021-10-24 02:34:30 -0400

    Second Version

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+class SnakePart{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
 let speed = 6;
 
 let tileCount = 20;
 let tileSize = canvas.width / tileCount - 2;
 let headX = 10;
 let headY = 10;
+const snakeParts = [];
+let tailLenght = 2;
 
 let appleX = 5;
 let appleY = 5;
@@ -14,16 +23,88 @@ let appleY = 5;
 let xVelocity=0;
 let yVelocity=0;
 
+let score = 0;
+
+const gulpSound = new Audio("gulp.mp3");
+
 //game loop
 function drawGame(){
-    clearScreen();
     changeSnakePosition();
+    let result = isGameOver();
+    if(result){
+        return;
+    }
+    
+    clearScreen();
+    
 
 
     checkAppleCollision();
     drawApple();
     drawSnake();
+
+    drawScore();
+
+    if(score > 2){
+        speed = 11;
+    }
+    if(score > 5){
+        speed = 15;
+    }
+
     setTimeout(drawGame, 1000/ speed);
+}
+
+function isGameOver(){
+    let gameOver = false;
+
+    if(yVelocity ===0 && xVelocity ===0){
+        return false;
+    }
+
+    //walls
+    if(headX < 0){
+        gameOver = true;
+    }
+    else if(headX === tileCount){
+        gameOver = true;
+    }
+    else if(headY < 0){
+        gameOver = true;
+    }
+    else if(headY === tileCount){
+        gameOver = true;
+    }
+
+    for(let i = 0; i < snakeParts.length; i++){
+        let part = snakeParts[i];
+        if(part.x === headX && part.y === headY){
+            gameOver = true;
+            break;
+        }
+    }
+
+    if(gameOver){
+        ctx.fillStyle = "white";
+        ctx.font = "50px Verdana";
+
+        var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+        gradient.addColorStop("0", "magenta");
+        gradient.addColorStop("0.5", "blue");
+        gradient.addColorStop("1.0", "red");
+
+        ctx.fillStyle = gradient;
+
+        ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+    }
+
+    return gameOver;
+}
+
+function drawScore(){
+    ctx.fillStyle = "white";
+    ctx.font = "10px Verdana";
+    ctx.fillText("Score " + score, canvas.width-50, 10);
 }
 
 function clearScreen(){
@@ -32,8 +113,23 @@ function clearScreen(){
 }
 
 function drawSnake(){
+    
+    
+
+    ctx.fillStyle = 'green';
+    for(let i = 0; i < snakeParts.length; i++){
+        let part = snakeParts[i];
+        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+    }
+
+    snakeParts.push(new SnakePart(headX, headY));
+    if(snakeParts.length > tailLenght){
+        snakeParts.shift();
+    }
+
     ctx.fillStyle = 'orange';
     ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+
 }
 
 function changeSnakePosition(){
@@ -50,6 +146,9 @@ function checkAppleCollision(){
     if(appleX == headX && appleY == headY){
         appleX = Math.floor(Math.random() * tileCount);
         appleY = Math.floor(Math.random() * tileCount);
+        tailLenght++;
+        score++;
+        gulpSound.play();
     }
 }
 
